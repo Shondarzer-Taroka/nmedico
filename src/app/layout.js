2854bc07125e1150aa651bf1d72b125e1bf5caed
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-08-25 00:08:24 +0600

    project starts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
+import NavBar from "@/components/NavBar/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          
+          <div>
+            <NavBar/>
+          </div>
+
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
